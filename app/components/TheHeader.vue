2fee9f54259ce4b0308a2fd75739578c6c877c0c
Ignat121 <ignatsergeewitch@ya.ru>
2026-02-06 18:29:02 +0700

    burger

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,35 +1,76 @@
 <template>
   <div class="container header">
     <DecorHeader />
-    <nav>
-      <ul class="header__list">
-        <li>Агентство</li>
-        <li>Услуги</li>
-        <li>Кейсы</li>
-        <li>Блог</li>
-        <li>Контакты</li>
+    <nav class="header__menu">
+      <ul class="header__menu__list">
+        <li><a href="">Агентство</a></li>
+        <li><a href="">Услуги</a></li>
+        <li><a href="">Кейсы</a></li>
+        <li><a href="">Блог</a></li>
+        <li><a href="">Контакты</a></li>
       </ul>
     </nav>
+
+    <button class="burger">
+      <span class="burger__line"></span>
+      <span class="burger__line"></span>
+      <span class="burger__line"></span>
+    </button>
   </div>
 </template>
 
 <style scoped lang="scss">
 .header {
   display: flex;
-  gap: 190px;
+  align-items: center;
+  gap: 191px;
   margin-top: 68px;
 
-  @include mobile {
-    display: none;
+  &__menu {
+    @include mobile {
+      position: fixed;
+      top: 0;
+      right: -100%;
+      width: 80%;
+      height: 100vh;
+      background: #333;
+      padding: 80px 20px 20px;
+      transition: right 0.5s ease;
+      z-index: 100;
+    }
+
+    &__list {
+      display: flex;
+      gap: 60px;
+      list-style: none;
+      color: $white;
+
+      @include mobile {
+        display: none;
+      }
+    }
   }
 
-  &__list {
-    display: flex;
-    gap: 60px;
-    list-style: none;
-    color: $white;
+  .burger {
+    display: none;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 30px;
+    height: 21px;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    padding: 0;
 
     @include mobile {
+      display: flex;
+    }
+
+    &__line {
+      width: 100%;
+      height: 3px;
+      background: $white;
+      transition: 0.3s;
     }
   }
 }
